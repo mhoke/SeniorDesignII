b21878bddbbcbe8826ab8c9092dcc36087bbbd88
msteppe <msteppe@Cant-Touch-This>
2013-02-21 12:23:38 -0500

    Splash screen goes to main menu

--- SeniorDesignII/src/edu/ycp/cs/Main/Window.java
+++ SeniorDesignII/src/edu/ycp/cs/Main/Window.java
@@ -11,53 +11,49 @@ public class Window extends JFrame
 {
 	private static final long serialVersionUID = 1L; //Dont know what this does either
 	
-	private JButton mainMenuButton = null;
+	private static void createAndShowGUI()  {
+		 
+		final JFrame mainmenu = new JFrame();
+        mainmenu.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        mainmenu.setSize(800, 600);
+        mainmenu.setResizable(false);
+        mainmenu.setTitle("M&M ARCADE");
+        mainmenu.setContentPane(new JLabel(new ImageIcon("M&M Arcade.png")));
+        mainmenu.setLayout(new FlowLayout());
+        mainmenu.setVisible(false);
 		
-	public Window() 
-	{
-		super(); //I don't know what this does
-		initialize();
-	}
-	
-	private void initialize() 
-	{
-		this.setSize(800, 600);
-		this.setResizable(false);
-		setTitle("M&M ARCADE");
-		setContentPane(new JLabel(new ImageIcon("Logo.png")));
-		setLayout(new FlowLayout());
-		
-		mainMenuButton = new JButton("Continue to Main Menu");
-		add(mainMenuButton);
-
-		this.addWindowListener(new java.awt.event.WindowAdapter() {
-			public void windowClosing(java.awt.event.WindowEvent e) {
-				System.exit(0);
-			}
-		});
-	}
-	
-	private JButton getDrawButton() 
-	{
-		if (mainMenuButton == null) 
-		{
-			mainMenuButton = new JButton();
-			mainMenuButton.setBounds(new java.awt.Rectangle(264,534,102,31));
-			mainMenuButton.setText("Draw!");
-			mainMenuButton.addActionListener(new java.awt.event.ActionListener() 
-			{
-				public void actionPerformed(java.awt.event.ActionEvent e) 
-				{
-					//Make the Jpanel change to main menu view
-				}
-			});
-		}
-		return mainMenuButton;
-	}
-	
-	public static void main(String args[])
-	{
-		Window window = new Window();
-		window.setVisible(true);
-	}
+		final JFrame splashscreen = new JFrame();
+        splashscreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        splashscreen.setSize(800, 600);
+        splashscreen.setResizable(false);
+        splashscreen.setTitle("M&M ARCADE");
+        splashscreen.setContentPane(new JLabel(new ImageIcon("Logo.png")));
+        splashscreen.setLayout(new FlowLayout());
+ 
+        JButton button = new JButton("Continue To Main Menu");
+        //Add action listener to button
+        button.addActionListener(new ActionListener() {
+ 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                splashscreen.setVisible(false);
+                mainmenu.setVisible(true);
+            }
+        });      
+ 
+        splashscreen.getContentPane().add(button);
+        splashscreen.pack();
+        splashscreen.setVisible(true);
+    }
+ 
+ 
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
