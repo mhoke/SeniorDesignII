eb40a58ddf04c55bce5b14e43ffc726de7ff9268
msteppe <msteppe@ycp.edu>
2013-03-16 12:47:12 -0400

    Button functionality on main menu screen starts the tetris game in console window. Need to make GUI for game yet.

--- SeniorDesignII/src/edu/ycp/cs/Main/MainMenuScreen.java
+++ SeniorDesignII/src/edu/ycp/cs/Main/MainMenuScreen.java
@@ -1,23 +1,55 @@
 package edu.ycp.cs.Main;
 
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class MainMenuScreen extends JPanel{
 	private static final long serialVersionUID = 1L;
 
 	private BufferedImage background;
+	//JPanel panelMainMenu;
+	//private JButton playButton, highScoreButton;
 
 	public MainMenuScreen() {
 		try {
 			background = ImageIO.read(new File("M&M Arcade.png"));
+//			setLayout(new GridBagLayout());
+//			
+//			panelMainMenu = this;
+//			playButton = new JButton("PLAY");
+//			highScoreButton = new JButton("HIGH SCORES");
+//			
+//			GridBagConstraints gbc = new GridBagConstraints();
+//			gbc.gridx = 0;
+//			gbc.gridy = 0;
+//			gbc.insets = new Insets(2,2,2,2);
+//			gbc.anchor = GridBagConstraints.CENTER;
+//			
+//			this.add(playButton, gbc);
+//			gbc.gridy++;
+//			this.add(highScoreButton, gbc);
+//			
+//			playButton.addActionListener(new java.awt.event.ActionListener() {
+//                public void actionPerformed(ActionEvent e) {
+//                    panelMainMenu.setVisible(false);
+//                }
+//            });
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- SeniorDesignII/src/edu/ycp/cs/Main/Window.java
+++ SeniorDesignII/src/edu/ycp/cs/Main/Window.java
@@ -3,23 +3,35 @@ package edu.ycp.cs.Main;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import edu.ycp.cs.Tetris.Game;
+
 public class Window {
-	JPanel cards; // a panel that uses CardLayout
+	JPanel cards;
 	final static String SPLASHSCREEN = "SplashScreen";
 	final static String MAINMENU = "MainMenu";
+	final static String TETRIS = "Tetris";
 
 	public void addComponentToWindow(Container pane) {
-		// Put the JComboBox in a JPanel to get a nicer look.
-		JPanel gameWindow = new JPanel(); // use FlowLayout
+		JPanel gameWindow = new JPanel(); // uses FlowLayout by default
 
 		// Create the "cards".
 		JPanel card1 = new JPanel();
+		//JPanel card2 = new MainMenuScreen();
+		JPanel card2 = new JPanel();
+		JPanel card3 = new JPanel();
+		
+		//Button for SplashScreen
 		JButton continueButton = new JButton("Continue");
 		continueButton.addActionListener(new ActionListener() {
 			@Override
@@ -28,20 +40,70 @@ public class Window {
 				cl.show(cards, MAINMENU);
 			}
 		});
-		card1.add(new SplashScreen());
-		card1.add(continueButton);
-
-		JPanel card2 = new JPanel();
-		JButton menuButton1 = new JButton("PLAY!");
+		
+		//SplashScreen setup
+		SplashScreen splash = new SplashScreen();
+		splash.setLayout(new FlowLayout());
+		card1.add(splash);
+		splash.add(continueButton);
+		
+		//Buttons for MainMenu
+		JButton menuButton1 = new JButton("PLAY!");	
+		menuButton1.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				CardLayout cl = (CardLayout) (cards.getLayout());
+				cl.show(cards, TETRIS);				
+			}
+		});
 		JButton menuButton2 = new JButton("HIGH SCORES");
-		card2.add(new MainMenuScreen());
-		card2.add(menuButton1);
-		card2.add(menuButton2);
+		
+		//MainMenu setup
+//		card2.setLayout(new GridBagLayout());		
+//		GridBagConstraints gbc = new GridBagConstraints();
+//		gbc.gridx = 0;
+//		gbc.gridy = 0;
+//		gbc.insets = new Insets(2,2,2,2);
+//		gbc.anchor = GridBagConstraints.CENTER;
+//		MainMenuScreen mms = new MainMenuScreen();
+//		mms.setLayout(new FlowLayout());
+//		card2.add(mms);
+//		card2.add(menuButton1, gbc);
+		
+		MainMenuScreen mms = new MainMenuScreen();
+		mms.setLayout(new FlowLayout());
+		card2.add(mms);
+		mms.add(menuButton1);
+		mms.add(menuButton2);
+		
+		//Tetris setup
+		JLabel label = new JLabel("TETRIS GAME WILL GO HERE");
+		JButton startGame = new JButton("START GAME");
+		card3.add(label);
+		card3.add(startGame);
+		startGame.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				//Game game = new Game();
+				try {
+					Game.main(null);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
 
+		//Sets up layout
 		cards = new JPanel(new CardLayout());
 		cards.add(card1, SPLASHSCREEN);
 		cards.add(card2, MAINMENU);
+		cards.add(card3, TETRIS);
 
+		//Creates the actual window
 		pane.add(gameWindow, BorderLayout.PAGE_START);
 		pane.add(cards, BorderLayout.CENTER);
 	}
