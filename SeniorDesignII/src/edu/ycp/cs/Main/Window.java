bfbd98de595d4795598df4e8d54b2e6b67e39087
msteppe <msteppe@ycp.edu>
2013-03-21 00:38:14 -0400

    Stuff

--- SeniorDesignII/src/edu/ycp/cs/Main/Window.java
+++ SeniorDesignII/src/edu/ycp/cs/Main/Window.java
@@ -17,7 +17,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.Timer;
+import javax.swing.event.TableModelEvent;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 import edu.ycp.cs.Tetris.Tetris;
 
@@ -31,6 +34,11 @@ public class Window {
 	final static int NUM_COLS = 10;
 	private JTable table = new JTable(new MyTableModel());
 	private Tetris game = new Tetris(); 
+//	private JTable table;
+//	private TableModel model;
+//	private int currentRow = 0;
+//  private int blockHeight = 3;
+//  private int blockWidth = 3;
 
 	public void addComponentToWindow(Container pane) {
 		// Create the "cards"
@@ -102,6 +110,66 @@ public class Window {
 				card3.revalidate(); //Redraws graphics
 			}
 		});
+			
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				card3.remove(0); //remove start button
+//				model = new TableModel();
+//				table = new JTable(model);
+//				table.setDefaultRenderer(Integer.class, new TableRenderer());
+//				table.setRowHeight(GRID_ROW_HEIGHT);
+//				for (int i = 0; i < NUM_COLS; i++) {
+//					table.getColumnModel().getColumn(i).setPreferredWidth(table.getRowHeight());					
+//				}
+//				card3.setLayout(new GridBagLayout());
+//				card3.add(table);
+//				//Tetris game = new Tetris();
+//				
+//				Timer timer = new Timer(500, new ActionListener() {					
+//					@Override
+//	                public void actionPerformed(ActionEvent e) {
+//	                    int col = (model.getColumnCount() - blockWidth) / 2;
+//	                    int row = currentRow - blockHeight;
+//	                    if (row + blockHeight >= model.getRowCount()) {
+//	                        ((Timer) e.getSource()).stop();
+//	                    } else {
+//	                        drawShape(row, col, 0);
+//	                        currentRow++;
+//	                        row = currentRow - blockHeight;
+//	                        drawShape(row, col, 3);
+//	                    }
+//	                }
+//					public void drawShape(int row, int col, int color) {
+//
+//	                    for (int index = 0; index < blockHeight; index++) {
+//
+//	                        if (row >= 0 && row < model.getRowCount()) {
+//
+//	                            switch (index) {
+//	                                case 0:
+//	                                case 1:
+//	                                    model.setValueAt(color, row, col);
+//	                                    break;
+//	                                case 2:
+//	                                    model.setValueAt(color, row, col);
+//	                                    model.setValueAt(color, row, col + 1);
+//	                                    model.setValueAt(color, row, col + 2);
+//	                                    break;
+//	                            }
+//
+//	                        }
+//	                        row++;
+//
+//	                    }
+//	                }
+//	            });
+//	            timer.setRepeats(true);
+//	            timer.setCoalesce(true);
+//	            timer.start();	            
+//	            
+//	            card3.revalidate();
+//			}
+//		});
 
 		// Sets up layout
 		cards = new JPanel(new CardLayout());
@@ -112,6 +180,65 @@ public class Window {
 		// Creates the actual window
 		pane.add(cards, BorderLayout.CENTER);
 	}
+	
+//	public class TableModel extends AbstractTableModel {
+//		
+//        private int[][] values;
+//
+//        public TableModel() {
+//            values = new int[NUM_ROWS][NUM_COLS];
+//        }
+//
+//        @Override
+//        public int getRowCount() {
+//            return NUM_ROWS;
+//        }
+//
+//        @Override
+//        public int getColumnCount() {
+//            return NUM_COLS;
+//        }
+//
+//        @Override
+//        public Class<?> getColumnClass(int columnIndex) {
+//            return Integer.class;
+//        }
+//
+//        @Override
+//        public Object getValueAt(int row, int col) {
+//            return values[row][col];
+//        }
+//
+//        @Override
+//        public void setValueAt(Object val, int row, int col) {
+//            values[row][col] = (Integer) val;
+//            fireTableCellUpdated(row, col);
+//        }
+//    }	
+//
+//	public class TableRenderer extends DefaultTableCellRenderer {
+//		@Override
+//        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+//            super.getTableCellRendererComponent(table, "", false, false, row, column);
+//            setOpaque(true);
+//            if (value != null) {
+//                if ((Integer) value == 0) {
+//                    setBackground(Color.WHITE);
+//                } else if ((Integer) value == 1) {
+//                    setBackground(Color.RED);
+//                } else if ((Integer) value == 2) {
+//                    setBackground(Color.GREEN);
+//                } else if ((Integer) value == 3) {
+//                    setBackground(Color.BLUE);
+//                } else if ((Integer) value == 4) {
+//                    setBackground(Color.YELLOW);
+//                }
+//            } else {
+//                setBackground(Color.DARK_GRAY);
+//            }
+//            return this;
+//        }
+//    }
 	
 	public Color getTableCellBackground(JTable table, int row, int col) {
         TableCellRenderer renderer = table.getCellRenderer(row, col);
